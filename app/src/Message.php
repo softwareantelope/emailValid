fd0c517f1b0638189b05e14fe88678deb62d17b5
Nicholas Alexander <nalex@sfsw.net>
2016-09-21 14:21:18 +0100

    initial commit
    first version is a demo only
    
    Signed-off-by: Nicholas Alexander <nalex@sfsw.net>
    
    readme
    
    welcome
    
    remove debug

---
+++ app/src/Message.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+class Message {
+
+    public function __construct() {
+    }
+
+    public static function welcome() {
+	$output = `cat ../resource/views/welcome.html`;
+	return $output;
+    }
+
+    public static function messageTo() {
+        $output = `cat ../resource/views/getEmailAddress.html`;
+        return $output;
+    }
+
+    public static function getReply($email, $link) {
+        $message = `cat ../resource/views/emailMessage.html`;
+        $message = preg_replace(['/%%email%%/', '/%%link%%/'], [$email, $link], $message);
+        return $message;
+    }
+}
